faa2c0260f99a1ca17fc8e5f2cdade8fe81f478f
ekankr2 <ekankr2@naver.com>
2025-02-04 01:52:06 +0900

    add package supabase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,9 +3,14 @@ import 'package:app/app/routes.dart';
 import 'package:app/app/style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://jdrtxofrlkcfxicyuejw.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpkcnR4b2ZybGtjZnhpY3l1ZWp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MDA2NjQsImV4cCI6MjA1NDE3NjY2NH0.L4C9of1LLsM84ATecAcNmqN-okh7HS9_r7CvRPZSNuw',
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
